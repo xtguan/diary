0f1183c63ae6cb4fd1fe155c164b72da0facd854
xguan43 <xguan43@gatech.edu>
2026-03-21 00:33:20 -0400

    2026-03-21 update gallery

--- src/posts/post-1.js
+++ src/posts/post-1.js
@@ -1,11 +1,11 @@
-import thumb from '../photos/tampa-clearwater-beach-1.jpg'
+import thumb from '../photos/2025-12-25-tampa/tampa-clearwater-beach-1.jpg'
 
 const content = `
 # My First Blog Post
 
 This is a sample blog post about a coastal trip.
 
-![coast](/src/photos/tampa-clearwater-beach-1.jpg)
+![coast](/src/photos/2025-12-25-tampa/tampa-clearwater-beach-1.jpg)
 
 More content here. Replace with your own writing.
 `
